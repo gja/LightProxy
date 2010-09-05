723d8e0967b8402075f750b37ec72e9c4d12faaf
Tejas Dinkar <tejas@gja.in>
2010-09-06 02:45:05 +0530

    Added an Invocation Class

---
+++ LightProxy/Invocation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace LightProxy
+{
+    public class Invocation : IInvocation
+    {
+        private readonly object backingObject;
+        private readonly Stack<IInterceptor> remainingInterceptors;
+
+        public Invocation(object backingObject, IEnumerable<IInterceptor> list, MethodInfo method, object[] arguments)
+        {
+            this.backingObject = backingObject;
+            remainingInterceptors = new Stack<IInterceptor>(list);
+            Method = method;
+            Arguments = arguments;
+        }
+
+        public MethodInfo Method { get; private set; }
+        public object[] Arguments { get; private set; }
+        public object ReturnValue { get; set; }
+        
+        public void Continue()
+        {
+            ReturnValue = Method.Invoke(backingObject, Arguments);
+        }
+    }
+
+    public interface IInvocation
+    {
+        MethodInfo Method { get; }
+        object[] Arguments { get; }
+        object ReturnValue { get; set; }
+
+        void Continue();
+    }
+}
---
+++ LightProxyTest/ExecutorTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using LightProxy;
+using NUnit.Framework;
+
+namespace LightProxyTest
+{
+    public interface IFace
+    {
+        int Foo();
+        bool Called { get; }
+    }
+
+    public class Face : IFace
+    {
+        public bool called;
+        public bool Called
+        {
+            get { return called; }
+        }
+
+        public int Foo() { called = true; return 10; }
+    }
+
+    [TestFixture]
+    public class ExecutorTest
+    {
+        [Test]
+        public void ShouldBeAbleToCreateAnExecutor()
+        {
+            var proxy = new ProxyBase<IFace>();
+            proxy.backingObject = new Face();
+            proxy.interceptors = new IInterceptor[0];
+
+            var invocation = new Invocation(proxy.backingObject, proxy.interceptors, typeof(IFace).GetMethods()[0], new object[0]);
+            invocation.Continue();
+            invocation.ReturnValue.ShouldBe(10);
+            proxy.backingObject.Called.ShouldBe(true);
+        }
+    }
+}
