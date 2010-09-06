8bf16b3752e46170483ae20f6f59b29019b55fd6
Tejas Dinkar <tejas@gja.in>
2010-09-07 02:14:33 +0530

    Constructing an invocation object each time

--- LightProxy/Internal/ProxyBase.cs
+++ LightProxy/Internal/ProxyBase.cs
@@ -8,13 +8,12 @@ namespace LightProxy.Internal
     {
         public T backingObject;
         public IInterceptor[] interceptors;
-        private Invocation invocation;
         protected MethodInfo[] methods;
 
         public object Execute(int i, object[] arguments, Func<object[], object> continueDelegate)
         {
             //TODO: Creating a new invocation each time seems to be marginally slower
-            return invocation.Start(methods[i], arguments, continueDelegate);
+            return new Invocation(interceptors).Start(methods[i], arguments, continueDelegate);
         }
 
         public void InitializeProxy(T backingObject, IInterceptor[] interceptors, MethodInfo[] methods)
@@ -22,7 +21,6 @@ namespace LightProxy.Internal
             this.backingObject = backingObject;
             this.interceptors = interceptors;
             this.methods = methods;
-            invocation = new Invocation(interceptors);
         }        
     }
 }
